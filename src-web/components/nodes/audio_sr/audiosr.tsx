28200fbe609f6ff8b75321be65d69abeddbde199
seruva19 <seruva19@yandex.ru>
2024-01-16 20:28:48 +0300

    rewrite 'audiosr' node as dynamic

--- src-web/components/nodes/audio_sr/audiosr.tsx
+++ src-web/components/nodes/audio_sr/audiosr.tsx
@@ -7,8 +7,34 @@ import ky from 'ky';
 
 const MecchiAudioSRNodeInfo = {
   type: 'audiosr',
+  title: 'AudioSR',
   group: 'convert',
-  view: MecchiAudioSRNode,
+
+  inputs: [{
+    name: 'sample',
+    type: 'sound',
+  }],
+  outputs: [{
+    name: 'sample',
+    type: 'sound',
+  }],
+
+  units: [{
+    name: "steps",
+    title: "Steps",
+    type: "number",
+    range: { min: 10, max: 300, step: 1 }
+  }, {
+    name: "guidance",
+    title: "Guidance scale",
+    type: "number",
+    range: { min: 1, max: 50, step: 1 }
+  }, {
+    name: "seed",
+    title: "Seed",
+    type: "number"
+  }],
+
   data: {
     steps: 100,
     guidance: 5,
@@ -38,44 +64,3 @@ const MecchiAudioSRNodeInfo = {
 
 
 export default MecchiAudioSRNodeInfo;
-
-const selector = (id: string) => (store: MecchiNodeStore) => ({
-  setSteps: (e: any) => store.updateNode(id, { steps: +e.target.value }),
-  setGuidance: (e: any) => store.updateNode(id, { guidance: +e.target.value }),
-  setSeed: (e: any) => store.updateNode(id, { seed: e.target.value }),
-});
-
-export function MecchiAudioSRNode({ id, data }: { id: string, data: any }) {
-  const { setSteps, setGuidance, setSeed } = useMecchiNodeStore(selector(id));
-
-  return <MecchiNode title="AudioSR" id={id}>
-    <Handle id="audiosr-a" className={`${tw`w-2 h-2 `}`} type="target" style={{ top: 40, bottom: 'auto', background: 'blue' }} position={Position.Left}>
-      <span className={`${tw`absolute font-bold text-xs ml-2 pl-1 rounded`}`} style={{ marginTop: -7 }}>sample</span>
-    </Handle>
-    <Handle id="audiosr-b" className={`${tw`w-2 h-2`}`} type="source" style={{ top: 30, bottom: 'auto', background: 'blue' }} position={Position.Right}>
-      <span className={`${tw`font-bold text-xs float-right mr-3`}`} style={{ marginTop: -7 }}>sample</span>
-    </Handle>
-
-    <div className={`${tw`flex flex-col p-2`}`}>
-      <div className={`${tw`flex`}`}>
-        <label className={tw('px-2 py-1 mb-2 gap-2 text-sm')}>Steps</label>
-        <input type="range" min="10" step="1" max="300" value={data.steps} onChange={setSteps}
-          className={tw('text-sm h-6 border-blue-200 border-1 rounded px-1 focus:outline-none')} />
-        <span className={`${tw`text-xs w-3 ml-1 mt-1 rounded`}`}>{data.steps}</span>
-      </div>
-
-      <div className={`${tw`flex`}`}>
-        <label className={tw('px-2 py-1 mb-2 gap-2 text-sm')}>Guidance scale</label>
-        <input type="range" min="1" step="1" max="50" value={data.guidance} onChange={setGuidance}
-          className={tw('text-sm h-6 border-blue-200 border-1 rounded px-1 focus:outline-none')} />
-        <span className={`${tw`text-xs w-3 ml-1 mt-1 rounded`}`}>{data.guidance}</span>
-      </div>
-
-      <div className={`${tw`flex`}`}>
-        <label className={tw('px-2 py-1 mb-2 gap-2 text-sm')}>Seed</label>
-        <input className={tw('text-sm h-6 flex-1 border-blue-200 border-1 rounded px-1 focus:outline-none')}
-          type="number" step="1" value={data.seed} onChange={setSeed} />
-      </div>
-    </div>
-  </MecchiNode>
-}; 
