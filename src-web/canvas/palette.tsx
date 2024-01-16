09eed6ec3f576cf1c563c52d16f61c4a041df107
seruva19 <seruva19@yandex.ru>
2024-01-16 20:24:49 +0300

    add list view of all nodes

--- src-web/canvas/palette.tsx
+++ src-web/canvas/palette.tsx
@@ -2,12 +2,13 @@ import { ControlButton, Panel } from 'reactflow'
 import { MecchiNodeStore, useMecchiNodeStore } from '../stores/node-store'
 import { shallow } from 'zustand/shallow'
 import { tw } from 'twind'
-import { useMecchiViewStore } from '../stores/view-store'
+import { useMecchiUIStore } from '../stores/ui-store'
 import { AiOutlineControl } from "react-icons/ai";
 import { GrStatusUnknown } from "react-icons/gr";
 import { MdOutlineElectricalServices } from "react-icons/md";
 import { MdOutlineMiscellaneousServices } from "react-icons/md";
 import { SiConvertio } from "react-icons/si";
+import { MdOutlineAllInclusive } from "react-icons/md";
 
 import { SlEnergy } from "react-icons/sl";
 import { useState } from 'react'
@@ -21,15 +22,15 @@ interface IProps {
 }
 
 export default function MecchiPalette({ nodeTypes }: IProps) {
-  const { paletteVisible } = useMecchiViewStore();
-  const [avNodes, setAvNodes] = useState<[string, string[]]>(['', []]);
+  const { paletteVisible } = useMecchiUIStore();
+  const [avNodes, setAvNodes] = useState<{ group: string, types: string[] }>({ group: '', types: [] });
 
   const onDragStart = (event: any, nodeType: string) => {
     event.dataTransfer.setData('application/reactflow', nodeType);
     event.dataTransfer.effectAllowed = 'move';
   };
 
-  const groups = Object.keys(nodeTypes);
+  const groups = ['all'].concat(Object.keys(nodeTypes));
   const group = (n: string) => <ControlButton style={{
     width: 100,
     fontSize: 13,
@@ -37,10 +38,14 @@ export default function MecchiPalette({ nodeTypes }: IProps) {
     justifyContent: 'left',
     border: '1px solid #eee',
     borderTopColor: 'transparent',
-    fontWeight: avNodes[0] == n ? 'bold' : 'initial',
+    fontWeight: avNodes.group == n ? 'bold' : 'initial',
     fontFamily: 'monospace'
   }} key={n} onClick={() => {
-    setAvNodes(avNodes[0] == n ? ['', []] : [n, nodeTypes[n]]);
+    if (n == 'all') {
+      setAvNodes(avNodes.group == 'all' ? { group: '', types: [] } : { group: 'all', types: (Object.values(nodeTypes) as string[][]).reduce((a, b) => a.concat(b)) });
+    } else {
+      setAvNodes(avNodes.group == n ? { group: '', types: [] } : { group: n, types: nodeTypes[n] });
+    }
   }} title={n} className={`${tw`hover:bg-blue-500 hover:text-white`}`}>
     <div className={tw`grid grid-flow-column`}>
       <div style={{ marginTop: 5, marginRight: 10 }}>
@@ -48,12 +53,12 @@ export default function MecchiPalette({ nodeTypes }: IProps) {
         {n == 'generate' && <div><SlEnergy /></div>}
         {n == 'convert' && <div><SiConvertio /></div>}
         {n == 'io' && <div><MdOutlineElectricalServices /></div>}
-        {n == undefined && <div><GrStatusUnknown /></div>}
+        {n == 'all' && <div><MdOutlineAllInclusive /></div>}
       </div>
       {n}
     </div></ControlButton>
 
-  const node = (nodeType: string) => (<ControlButton className={tw`grid grid-flow-column`} style={{
+  const node = (nodeGroup: string, nodeType: string) => (<ControlButton className={tw`grid grid-flow-column`} style={{
     width: 200,
     fontSize: 13,
     outline: 'none',
@@ -61,12 +66,12 @@ export default function MecchiPalette({ nodeTypes }: IProps) {
     border: '1px solid #eee',
     borderTopColor: 'transparent',
     fontFamily: 'monospace'
-  }} key={nodeType} onDragStart={(event) => onDragStart(event, nodeType)} draggable>node: {nodeType}</ControlButton>)
+  }} key={nodeType} onDragStart={(event) => onDragStart(event, nodeType)} draggable>{nodeType}</ControlButton>)
 
   return (
     <>
       <Global
-        styles={css`.mecchi-palette .react-flow__controls-button:first-child {
+        styles={css`.mecchi-palette .react-flow__controls-button:first-of-type {
           border-top-color: #eee !important;
       `}
       />
@@ -78,12 +83,16 @@ export default function MecchiPalette({ nodeTypes }: IProps) {
         </div>
 
         <div style={{
-          'display': avNodes[1].length != 0 ? 'block' : 'none',
+          'display': avNodes.types.length != 0 ? 'block' : 'none',
           position: 'absolute',
           top: 0,
           left: 120
-        }}><div>
-            {avNodes[1]!.map(node)}
+        }}><div style={{
+          maxHeight: 'calc(100% - 30px)',
+          position: 'fixed',
+          overflowY: 'auto'
+        }}>
+            {avNodes.types.map(t => node(avNodes.group, t))}
           </div>
         </div>
 
