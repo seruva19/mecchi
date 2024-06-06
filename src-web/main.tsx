7bb97833c2bdb1a08cdb7901a4a16317be08aa20
seruva19 <seruva19@yandex.ru>
2024-06-07 01:09:28 +0300

    add shared CSS styles

--- src-web/main.tsx
+++ src-web/main.tsx
@@ -5,9 +5,12 @@ import MecchiCanvas from './workflow/canvas'
 import { KBarAnimator, KBarPortal, KBarPositioner, KBarProvider, KBarResults, KBarSearch, useMatches } from "kbar";
 import { RenderResults } from './workflow/command-bar/results';
 import { actions } from './workflow/command-bar/kbar';
+import { Global, css } from '@emotion/react';
+import { globalStyles } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <>
+    <Global styles={globalStyles}></Global>
     <React.StrictMode>
       <KBarProvider actions={actions}>
         <KBarPortal>
---
+++ src-web/styles.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+.mecchi-btn {
+  font-size: 13px;
+  outline: none !important;
+  border-radius: 10px;
+  background: aliceblue;
+  padding: 0 10px;
+}
+`
+
+export const tooltipStyles = { fontSize: 11, padding: "0 10px", background: 'aliceblue', color: 'black', textAlign: 'center' }
